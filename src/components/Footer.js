3bd84e411c9f3e0961c230580add597f155df1aa
Nya Tyus <nyastyus@gmail.com>
2023-06-15 16:32:28 -0400

    style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,29 @@
 import React from "react";
-
-const styles = {
-  logoStyles: {
-    width: "2rem",
-  },
-};
+import { Link } from "react-router-dom";
+import "../styles/Footer.css";
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      <div className="content has-text-centered">
-        <a href="https://www.linkedin.com/in/nyatyus/">
-          <img src="../images/linkedin.png" alt="LinkedIn logo" style={styles.logoStyles}></img>
-        </a>
-        <a href="https://github.com/nyashanice">
-          <img src="../../images/github.png" alt="GitHub logo" style={styles.logoStyles}></img>
-        </a>
+    <footer className="footer text-center p-3">
+      <div className="text-center footer-text">Contact me!</div>
+      <div className="">
+        <Link to="mailto:nyastyus@gmail.com">
+          <img src="../images/email.png" alt="" className="footer-logo m-2" />
+        </Link>
+        <Link to="https://www.linkedin.com/in/nyatyus/">
+          <img
+            src="../images/linkedin.png"
+            alt="LinkedIn logo"
+            className="footer-logo m-2"
+          />
+        </Link>
+        <Link to="https://github.com/nyashanice">
+          <img
+            src="../../images/github.png"
+            alt="GitHub logo"
+            className="footer-logo m-2"
+          />
+        </Link>
       </div>
     </footer>
   );
