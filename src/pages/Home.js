c838fc437e18a3cb7ba691835a0ec67d534a81e2
Nya Tyus <nyastyus@gmail.com>
2023-06-15 18:48:53 -0400

    create tablet media query for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,46 +4,54 @@ import "../styles/Home.css";
 
 export default function Home() {
   return (
-    <div className="home-bg">
-      <div className="home-name p-2 pb-0 pt-5">Nya Tyus</div>
-      {/* <div className="home-subtitle p-2 pt-0">
+    <div className="home-bg home-cols">
+      <div className="home-left">
+        <div className="home-name p-2 pb-0 pt-5">Nya Tyus</div>
+        {/* <div className="home-subtitle p-2 pt-0">
         AKA Tinker Bell of the tech world
       </div> */}
-      <div className="home-title p-2">[Web Developer]</div>
-      <ul className="home-nav-list text-center p-0 mt-3">
-        <Link to="/about" className="home-list-item">
-          <li className="p-3">About</li>
-        </Link>
-        <Link to="/portfolio" className="home-list-item">
-          <li className="p-3">Portfolio</li>
-        </Link>
-        <Link
-          to="https://docs.google.com/document/d/1CxE84TLEQTlU-VEgh5_n1IS3XcxpDtly30RFOQFtGHo/edit?usp=sharing"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="home-list-item"
-        >
-          <li className="p-3">Resume</li>
-        </Link>
-      </ul>
-      <div className="d-flex flex-row justify-content-center">
-        <Link to="mailto:nyastyus@gmail.com">
-          <img src="../images/email.png" alt="" className="home-icon m-2" />
-        </Link>
-        <Link
-          to="https://www.linkedin.com/in/nyatyus/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="../images/linkedin.png" alt="" className="home-icon m-2" />
-        </Link>
-        <Link
-          to="https://github.com/nyashanice"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="../images/github.png" alt="" className="home-icon m-2" />
-        </Link>
+        <div className="home-title p-2">[Web Developer]</div>
+      </div>
+      <div className="home-right">
+        <ul className="home-nav-list text-center p-0 mt-3">
+          <Link to="/about" className="home-list-item">
+            <li className="p-3">About</li>
+          </Link>
+          <Link to="/portfolio" className="home-list-item">
+            <li className="p-3">Portfolio</li>
+          </Link>
+          <Link
+            to="https://docs.google.com/document/d/1CxE84TLEQTlU-VEgh5_n1IS3XcxpDtly30RFOQFtGHo/edit?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="home-list-item"
+          >
+            <li className="p-3">Resume</li>
+          </Link>
+        </ul>
+        <div className="d-flex flex-row justify-content-center">
+          <Link to="mailto:nyastyus@gmail.com">
+            <img src="../images/email.png" alt="" className="home-icon m-2" />
+          </Link>
+          <Link
+            to="https://www.linkedin.com/in/nyatyus/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="../images/linkedin.png"
+              alt=""
+              className="home-icon m-2"
+            />
+          </Link>
+          <Link
+            to="https://github.com/nyashanice"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="../images/github.png" alt="" className="home-icon m-2" />
+          </Link>
+        </div>
       </div>
     </div>
   );
