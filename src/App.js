7e00852c0ac36b9e9c6fe3524504d448f6a6f61c
Nya Tyus <nyastyus@gmail.com>
2023-06-16 03:43:52 -0400

    reverting to 424fc42e

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import AboutMe from "./pages/AboutMe";
+import Portfolio from "./pages/Portfolio";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutMe />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+          </Routes>
+      </div>
+    </Router>
+  );
+}
