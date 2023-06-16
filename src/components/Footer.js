95973d85c56d3f4e3e90fd2a749f3c7539f26bb1
Nya Tyus <nyastyus@gmail.com>
2023-06-16 02:49:58 -0400

    reverting to 424fc42 because of mistake adding master branch connecting to gh pages

---
+++ src/components/Footer.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer text-center p-3">
+      <div className="text-center footer-text">Contact me!</div>
+      <div className="">
+        <Link to="mailto:nyastyus@gmail.com">
+          <img src="../images/email.png" alt="" className="footer-logo m-2" />
+        </Link>
+        <Link
+          to="https://www.linkedin.com/in/nyatyus/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="../images/linkedin.png"
+            alt="LinkedIn logo"
+            className="footer-logo m-2"
+          />
+        </Link>
+        <Link
+          to="https://github.com/nyashanice"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="../../images/github.png"
+            alt="GitHub logo"
+            className="footer-logo m-2"
+          />
+        </Link>
+      </div>
+    </footer>
+  );
+}
