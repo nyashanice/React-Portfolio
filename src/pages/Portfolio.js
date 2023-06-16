424fc42ed758294ea4a5e4d98139974dd247310e
Nya Tyus <nyastyus@gmail.com>
2023-06-15 20:16:31 -0400

    change portfolio order

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -14,68 +14,6 @@ export default function Portfolio() {
       <div className="portfolio-contents">
         <div className="portfolio-row">
           <div className="m-2 p-1 project-card">
-            <h2 className="m-0 p-0 project-title">Shot Clock</h2>
-            <p className="m-1 p-0 project-text">
-              Shot Clock is an NBA game predictor website that also shows users
-              previous season statistics. You login, choose a team, and predict
-              the game outcome versus any other team!{" "}
-            </p>
-            <p className="m-0 p-0 project-stack">
-              MySQL, Handlebars, JavaScript, Express, Node, Bulma
-            </p>
-            <p className="m-0 p-0 text-muted project-text contributors">
-              Contributors: Nya Tyus, Seve Nerowski, Teresa Rodriguez, Jillian
-              Hallmark, Nick Fierke
-            </p>
-            <div className="d-flex flex-row justify-content-around m-0 p-0">
-              <Link
-                to="https://github.com/nyashanice/SHOT_CLOCK"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="project-link"
-              >
-                GitHub
-              </Link>
-              <Link
-                to="https://nyashanice.github.io/SHOT_CLOCK/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="project-link"
-              >
-                Deployed
-              </Link>
-            </div>
-          </div>
-          <div className="m-2 p-1 project-card">
-            <h2 className="m-0 p-0 project-title">Voyage Vistas</h2>
-            <p className="m-1 p-0 project-text">
-              Voyage Vistas Travel Agency website showcases the services offered
-              by the business, testimonials, and a inquiry form to fill out for
-              interested visitors.
-            </p>
-            <p className="m-0 p-0 project-stack">React, Bootstrap</p>
-            <div className="d-flex flex-row justify-content-around m-0 p-0">
-              <Link
-                to="https://github.com/nyashanice/Travel-Agent-Site"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="project-link"
-              >
-                GitHub
-              </Link>
-              <Link
-                to="https://nyashanice.github.io/Travel-Agent-Site/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="project-link"
-              >
-                Deployed
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className="portfolio-row">
-          <div className="m-2 p-1 project-card">
             <h2 className="m-0 p-0 project-title">The Faithful Pixel</h2>
             <p className="m-1 p-0 project-text">
               The Faithful Pixel website is for freelance web development
@@ -122,6 +60,68 @@ export default function Portfolio() {
               </Link>
               <Link
                 to="https://nyashanice.github.io/Fast-Twitch-Frenzy/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project-link"
+              >
+                Deployed
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="portfolio-row">
+          <div className="m-2 p-1 project-card">
+            <h2 className="m-0 p-0 project-title">Voyage Vistas</h2>
+            <p className="m-1 p-0 project-text">
+              Voyage Vistas Travel Agency website showcases the services offered
+              by the business, testimonials, and an inquiry form to fill out for
+              interested visitors.
+            </p>
+            <p className="m-0 p-0 project-stack">React, Bootstrap</p>
+            <div className="d-flex flex-row justify-content-around m-0 p-0">
+              <Link
+                to="https://github.com/nyashanice/Travel-Agent-Site"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project-link"
+              >
+                GitHub
+              </Link>
+              <Link
+                to="https://nyashanice.github.io/Travel-Agent-Site/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project-link"
+              >
+                Deployed
+              </Link>
+            </div>
+          </div>
+          <div className="m-2 p-1 project-card">
+            <h2 className="m-0 p-0 project-title">Shot Clock</h2>
+            <p className="m-1 p-0 project-text">
+              Shot Clock is an NBA game predictor website that also shows users
+              previous season statistics. You login, choose a team, and predict
+              the game outcome versus any other team!{" "}
+            </p>
+            <p className="m-0 p-0 project-stack">
+              MySQL, Handlebars, JavaScript, Express, Node, Bulma
+            </p>
+            <p className="m-0 p-0 text-muted project-text contributors">
+              Contributors: Nya Tyus, Seve Nerowski, Teresa Rodriguez, Jillian
+              Hallmark, Nick Fierke
+            </p>
+            <div className="d-flex flex-row justify-content-around m-0 p-0">
+              <Link
+                to="https://github.com/nyashanice/SHOT_CLOCK"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project-link"
+              >
+                GitHub
+              </Link>
+              <Link
+                to="https://nyashanice.github.io/SHOT_CLOCK/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="project-link"
